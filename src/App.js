d1b5fa49d6c77727ffa4ca7d7b6fa1cb79304485
Darius <chapd289@robertmorris.edu>
2020-08-23 14:29:28 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Welcome from './components/welcome/Welcome';
+import Clock from './components/clock/Clock';
+import Contact from './components/contact/Contact';
+import Navigation from './components/navigation/Navigation';
+
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navigation />
+     
+     <Switch>
+     <Route 
+     exact 
+     path="/" exact 
+     render={(props) => <Welcome {...props} name="Darius" />} 
+     />
+     {/* <Route path= "welcome/:name" component={Name} /> */}
+     <Route exact path="/clock" exact component={Clock}/>
+     <Route exact path="/contact" exact component={Contact}/>
+     <Route path="/" render={() => <h1>404 No Match Found</h1>} />
+    </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
