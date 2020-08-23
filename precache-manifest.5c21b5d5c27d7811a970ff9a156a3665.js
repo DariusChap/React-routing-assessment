38d1aa38222344fb5e689af39e36abb9c5b33fb1
Darius <chapd289@robertmorris.edu>
2020-08-23 14:37:47 -0500

    Updates

---
+++ precache-manifest.5c21b5d5c27d7811a970ff9a156a3665.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803bce8f66b5d8c5426f0c555fef5991",
+    "url": "/React-routing-assessment/index.html"
+  },
+  {
+    "revision": "17236234fde1e6b21b68",
+    "url": "/React-routing-assessment/static/css/main.d4f213fd.chunk.css"
+  },
+  {
+    "revision": "11bfc443377b04d9d1dd",
+    "url": "/React-routing-assessment/static/js/2.8862826c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-routing-assessment/static/js/2.8862826c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17236234fde1e6b21b68",
+    "url": "/React-routing-assessment/static/js/main.27d03ef1.chunk.js"
+  },
+  {
+    "revision": "acdf79544cfb8a0832fe",
+    "url": "/React-routing-assessment/static/js/runtime-main.433ff1b0.js"
+  }
+]);
